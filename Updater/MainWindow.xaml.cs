2e9f8c1b9fb18a9eaa92ef4e87fadf40450c6829
Vlad <53918010+VladimirUE4@users.noreply.github.com>
2021-09-18 17:42:31 +0200

    Update

---
+++ Updater/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Diagnostics;
+
+
+namespace Updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            WebClient client = new WebClient();
+
+            try
+            {
+                client.DownloadFile("http://localhost/Dofus/bones345.d2p", @"C:\Users\jocel\Desktop\Test\bones345.d2p");
+                Console.WriteLine("Download");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Erreur");
+                Environment.Exit(0);
+            }
+            client.Dispose();
+
+
+            Environment.Exit(0);
+
+        }
+    }
+}
